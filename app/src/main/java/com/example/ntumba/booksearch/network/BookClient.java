7611dbc6d195dd6386b39e5a7034884e57689be1
Elie nsampi <elientumba@live.fr>
2017-10-31 23:31:03 +0800

    implementention of the client finished

--- app/src/main/java/com/example/ntumba/booksearch/network/BookClient.java
+++ app/src/main/java/com/example/ntumba/booksearch/network/BookClient.java
@@ -1,8 +1,58 @@
 package com.example.ntumba.booksearch.network;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * Created by ntumba on 17-10-31.
  */
 
 public class BookClient {
+
+    private static final String API_BASE_URL = "http://openlibrary.org/";
+    private AsyncHttpClient client;
+
+
+    /**
+     * constructor
+     */
+    public BookClient(){
+        this.client = new AsyncHttpClient();
+    }
+
+
+
+
+
+    /**
+     * return the api url
+     * @param relativeUrl
+     * @return
+     */
+    public String getApiUrl(String relativeUrl){
+        return API_BASE_URL + relativeUrl;
+    }
+
+
+
+
+    /**
+     * method fo accessing the search
+     * @param query
+     * @param handler
+     */
+    public void getBooks(final String query , JsonHttpResponseHandler handler){
+        try{
+
+            String url = getApiUrl("search.json?q=");
+            client.get(url + URLEncoder.encode(query , "utf-8") , handler);
+
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+    }
 }
+
