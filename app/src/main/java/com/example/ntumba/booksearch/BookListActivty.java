837875c64e32b615cd8eae138a1d45b34b4cf636
Elie nsampi <elientumba@live.fr>
2017-10-31 22:57:32 +0800

    creating the Book list activity and make it extend the single activity

---
+++ app/src/main/java/com/example/ntumba/booksearch/BookListActivty.java
@@ -0,0 +1,20 @@
+package com.example.ntumba.booksearch;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ntumba on 17-10-31.
+ */
+
+public class BookListActivty extends SingleMainActivity {
+
+
+    /**
+     * returns the activity fragment
+     * @return
+     */
+    @Override
+    public Fragment makeFragment() {
+        return null;
+    }
+}
