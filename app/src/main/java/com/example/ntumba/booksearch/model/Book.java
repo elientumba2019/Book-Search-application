2099281843054de5946b830228a1b027435d8eab
Elie nsampi <elientumba@live.fr>
2017-10-31 23:38:54 +0800

    end implementation of the model

--- app/src/main/java/com/example/ntumba/booksearch/model/Book.java
+++ app/src/main/java/com/example/ntumba/booksearch/model/Book.java
@@ -1,8 +1,55 @@
 package com.example.ntumba.booksearch.model;
 
+import java.io.Serializable;
+
 /**
  * Created by ntumba on 17-10-31.
  */
 
-public class Book {
+public class Book implements Serializable {
+
+    private String libraryId;
+    private String author;
+    private String title;
+
+
+    public Book(String id , String author , String title){
+        this.libraryId = id;
+        this.author = author;
+        this.title = title;
+    }
+
+
+    public String getLibraryId() {
+        return libraryId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+
+    /**
+     * return the sized book cover from cover API
+     * @return
+     */
+    public String getCoverUrl(){
+        String url ="http://covers.openlibrary.org/b/olid/" + libraryId + "-M.jpg?default=false";
+        return  url;
+    }
+
+
+    /**
+     * return the large sized cover from the API
+     * @return
+     */
+    public String getLargeCoverUrl(){
+        String url = "http://covers.openlibrary.org/b/olid/" + libraryId + "-L.jpg?default=false";
+        return  url;
+    }
 }
