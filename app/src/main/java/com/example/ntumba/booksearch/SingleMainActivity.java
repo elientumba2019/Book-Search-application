5a8a44c19f0bb98068c1aa801af32bee2624ee7f
Elie nsampi <elientumba@live.fr>
2017-10-31 22:51:28 +0800

    renaming the main activity

--- app/src/main/java/com/example/ntumba/booksearch/SingleMainActivity.java
+++ app/src/main/java/com/example/ntumba/booksearch/SingleMainActivity.java
@@ -3,11 +3,11 @@ package com.example.ntumba.booksearch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SingleMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.single_main_layout);
     }
 }
