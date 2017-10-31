24e310ab8efa4fc1e0ded2bcab4ae3068016fdc4
Elie nsampi <elientumba@live.fr>
2017-10-31 22:55:16 +0800

    making the activity abstract and implement the mechanism fo puttng a fragment dunamically on screen

--- app/src/main/java/com/example/ntumba/booksearch/SingleMainActivity.java
+++ app/src/main/java/com/example/ntumba/booksearch/SingleMainActivity.java
@@ -1,13 +1,37 @@
 package com.example.ntumba.booksearch;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SingleMainActivity extends AppCompatActivity {
+public abstract class SingleMainActivity extends AppCompatActivity {
+
+
+    /**
+     * returns a fragment from an activity
+     * that extends this class
+     * @return
+     */
+    public abstract Fragment makeFragment();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_main_layout);
+
+
+        //getting manager
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.general_container);
+
+
+        //putting on screen
+        if(fragment == null){
+            fragment = makeFragment();
+            manager.beginTransaction().add(R.id.general_container , fragment).commit();
+        }
     }
 }
