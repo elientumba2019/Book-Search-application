9cb3d035591e90b64f4e6f5ce7f2d8fbf2be7829
Elie nsampi <elientumba@live.fr>
2017-11-02 11:30:28 +0800

    implementing get from Json method whicj takes a json object as argument and deserialize it into a book object

--- app/src/main/java/com/example/ntumba/booksearch/model/Book.java
+++ app/src/main/java/com/example/ntumba/booksearch/model/Book.java
@@ -1,5 +1,9 @@
 package com.example.ntumba.booksearch.model;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 
 /**
@@ -11,6 +15,9 @@ public class Book implements Serializable {
     private String libraryId;
     private String author;
     private String title;
+
+
+    public Book(){}
 
 
     public Book(String id , String author , String title){
@@ -53,6 +60,57 @@ public class Book implements Serializable {
         String url = "http://covers.openlibrary.org/b/olid/" + libraryId + "-L.jpg?default=false";
         return  url;
     }
+
+
+
+
+    /**
+     * returns the book object given the JSON object
+     * will take the object and deserialize it
+     * @param object
+     */
+    public static Book fromJson(JSONObject object){
+
+        Book book = new Book();
+
+        try{
+
+            //deserialize json into object fields and assign
+            //those fields to the book object
+            //checks
+            if(object.has("cover_edition_key")){
+                book.libraryId = object.getString("cover_edition_key");
+            }
+
+            else if(object.has("edition_key")){
+                final JSONArray ids = object.getJSONArray("edition_key");
+                book.libraryId = ids.getString(0);
+            }
+
+            book.title = object.has("title_suggest") ? object.getString("title_suggest") : "";
+            book.author = getAuthor(object);
+
+
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return book;
+    }
+
+
+    /**
+     * given a json object corresponding to the book
+     * will retrieve the author of the book
+     * or authors separated with commas if there are more than one
+     * @param object
+     * @return
+     */
+    public static String getAuthor(JSONObject object){
+
+    }
+
 
 
 
