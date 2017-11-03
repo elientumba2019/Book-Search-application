52816e7b92d123ce97b132418affe72d011f4ead
Elie nsampi <elientumba@live.fr>
2017-11-03 20:37:11 +0800

    Implementation of the view holder constructor

--- app/src/main/java/com/example/ntumba/booksearch/listUtils/BookViewHolder.java
+++ app/src/main/java/com/example/ntumba/booksearch/listUtils/BookViewHolder.java
@@ -1,8 +1,31 @@
 package com.example.ntumba.booksearch.listUtils;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.ntumba.booksearch.databinding.BookSingleItemBinding;
+
 /**
  * Created by ntumba on 17-11-2.
  */
 
-public class BookViewHolder {
+public class BookViewHolder extends RecyclerView.ViewHolder{
+
+    private Context context;
+    private BookSingleItemBinding item;
+
+
+
+    /**
+     * Constructor of the view
+     * holder
+     * @param context
+     * @param item
+     */
+    public BookViewHolder(Context context , BookSingleItemBinding item) {
+        super(item.getRoot());
+        this.context = context;
+        this.item = item;
+    }
 }
