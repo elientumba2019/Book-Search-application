d6ffc332055ca021d88dee786caa76520a4cfa73
Elie nsampi <elientumba@live.fr>
2017-11-03 21:02:12 +0800

    Implementing the adapter

--- app/src/main/java/com/example/ntumba/booksearch/listUtils/BookAdapter.java
+++ app/src/main/java/com/example/ntumba/booksearch/listUtils/BookAdapter.java
@@ -1,8 +1,68 @@
 package com.example.ntumba.booksearch.listUtils;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.ntumba.booksearch.R;
+import com.example.ntumba.booksearch.databinding.BookSingleItemBinding;
+import com.example.ntumba.booksearch.model.Book;
+
+import java.util.ArrayList;
+
 /**
  * Created by ntumba on 17-11-2.
  */
 
-public class BookAdapter {
+public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
+
+
+    private ArrayList<Book> list;
+    private Context context;
+
+
+
+
+    public BookAdapter(){}
+
+
+    /**
+     * constructor
+     * @param context
+     * @param list
+     */
+    public BookAdapter(Context context , ArrayList<Book> list){
+        this.list = list;
+        this.context = context;
+    }
+
+
+    /**
+     * returns an instance of the view holder
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        BookSingleItemBinding item = DataBindingUtil.inflate(inflater , R.layout.book_single_item , parent , false);
+        return new BookViewHolder(context , item);
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        holder.bind(list.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 }
--- app/src/main/java/com/example/ntumba/booksearch/listUtils/BookViewHolder.java
+++ app/src/main/java/com/example/ntumba/booksearch/listUtils/BookViewHolder.java
@@ -1,10 +1,14 @@
 package com.example.ntumba.booksearch.listUtils;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.ntumba.booksearch.R;
 import com.example.ntumba.booksearch.databinding.BookSingleItemBinding;
+import com.example.ntumba.booksearch.model.Book;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by ntumba on 17-11-2.
@@ -27,5 +31,19 @@ public class BookViewHolder extends RecyclerView.ViewHolder{
         super(item.getRoot());
         this.context = context;
         this.item = item;
+    }
+
+
+
+
+    /**
+     *
+     * @param book
+     */
+    public void bind(Book book){
+        item.tvTitle.setText(book.getTitle());
+        item.tvAuthor.setText(book.getAuthor());
+        Picasso.with(context).load(Uri.parse(book.getCoverUrl())).error(R.drawable.no_cover).into(item.ivBookCover);
+
     }
 }
