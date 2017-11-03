fab7373c7c3132f551b2d3732869a7654b17b7a7
Elie nsampi <elientumba@live.fr>
2017-11-03 21:07:05 +0800

    adding necessary calls to place the list on screen

--- app/src/main/java/com/example/ntumba/booksearch/fragments/BookListFragment.java
+++ app/src/main/java/com/example/ntumba/booksearch/fragments/BookListFragment.java
@@ -1,11 +1,18 @@
 package com.example.ntumba.booksearch.fragments;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ntumba.booksearch.R;
+import com.example.ntumba.booksearch.databinding.BookListFragmentLayoutBinding;
+import com.example.ntumba.booksearch.listUtils.BookAdapter;
+import com.example.ntumba.booksearch.model.Book;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +20,10 @@ import com.example.ntumba.booksearch.R;
  */
 
 public class BookListFragment extends Fragment {
+
+
+    private BookListFragmentLayoutBinding fragment;
+    private BookAdapter adapter;
 
 
 
@@ -29,8 +40,13 @@ public class BookListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.book_list_fragment_layout , container , false);
-        return view;
+
+        fragment = DataBindingUtil.inflate(inflater , R.layout.book_list_fragment_layout , container , false);
+        fragment.bookList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new BookAdapter(getContext() , new ArrayList<Book>());
+        fragment.bookList.setAdapter(adapter);
+
+        return fragment.getRoot();
     }
 
 
