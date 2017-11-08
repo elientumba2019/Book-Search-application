3941763fa3bf34955e625bcfbefb9509884e4a2b
Elie nsampi <elientumba@live.fr>
2017-11-08 08:21:55 +0800

    Adding the book detail fragment corresponding to the activity

---
+++ app/src/main/java/com/example/ntumba/booksearch/fragments/BookDetailsFragment.java
@@ -0,0 +1,10 @@
+package com.example.ntumba.booksearch.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ntumba on 17-11-8.
+ */
+
+public class BookDetailsFragment extends Fragment {
+}
