5ed0d9c77cac38014882216c129e7caefb6a0c73
Elie nsampi <elientumba@live.fr>
2017-11-08 08:19:48 +0800

    adding the book detail activity to be a details view for each book

---
+++ app/src/main/java/com/example/ntumba/booksearch/activities/BookDetailsActivity.java
@@ -0,0 +1,21 @@
+package com.example.ntumba.booksearch.activities;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ntumba on 17-11-8.
+ */
+
+public class BookDetailsActivity extends SingleMainActivity {
+
+
+
+    /**
+     * returns the coresponding fragment
+     * @return
+     */
+    @Override
+    public Fragment makeFragment() {
+        return null;
+    }
+}
