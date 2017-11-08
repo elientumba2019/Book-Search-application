08bb78831a7481bce6511ec9a10b3e1177058ac6
Elie nsampi <elientumba@live.fr>
2017-11-08 09:50:39 +0800

    Populating the detail view

--- app/src/main/java/com/example/ntumba/booksearch/fragments/BookDetailsFragment.java
+++ app/src/main/java/com/example/ntumba/booksearch/fragments/BookDetailsFragment.java
@@ -1,16 +1,28 @@
 package com.example.ntumba.booksearch.fragments;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.ntumba.booksearch.R;
 import com.example.ntumba.booksearch.databinding.BookDetailFragmentBinding;
 import com.example.ntumba.booksearch.model.Book;
+import com.example.ntumba.booksearch.network.BookClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by ntumba on 17-11-8.
@@ -20,10 +32,19 @@ public class BookDetailsFragment extends Fragment {
 
 
     private static final String ARGUMENT_KEY = "key";
+    private Book book;
+    private BookClient client;
 
 
     public BookDetailsFragment(){}
 
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        book  = (Book) getArguments().getSerializable(ARGUMENT_KEY);
+    }
 
 
 
@@ -38,7 +59,69 @@ public class BookDetailsFragment extends Fragment {
 
         BookDetailFragmentBinding binding;
         binding = DataBindingUtil.inflate(inflater , R.layout.book_detail_fragment , container , false);
+
+        if(book == null){
+            Toast.makeText(getActivity() , "The book details couldn't load" , Toast.LENGTH_LONG).show();
+        }
+        else{
+            loadBook(book , binding);
+        }
+
         return  binding.getRoot();
+    }
+
+
+
+
+    /**
+     * loads the books details and displays them on screes
+     * @param book
+     */
+    private void loadBook(Book book , BookDetailFragmentBinding binding){
+
+        getActivity().setTitle(book.getTitle());
+
+        Picasso.with(getActivity()).load(Uri.parse(book.getCoverUrl())).error(R.drawable.no_cover).into(binding.ivBookCover);
+        binding.tvTitle.setText(book.getTitle());
+        binding.tvAuthor.setText(book.getAuthor());
+
+
+        client = new BookClient();
+        client.getExtraBookDetail(book.getLibraryId() , new JsonHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode , Header[] headers , JSONObject response){
+
+                try{
+
+                    if(response.has("publishers")){
+                        //display comma separated list of publishers
+                        final JSONArray publisher = response.getJSONArray("publishers");
+                        final int numPublichser = publisher.length();
+
+                        final String[] publishers = new String[numPublichser];
+
+                        for(int c = 0 ; c < numPublichser ; c++){
+                            publishers[c] = publisher.getString(c);
+                        }
+
+                        binding.tvPublisher.setText(TextUtils.join(", " , publishers));
+                    }
+
+                    if(response.has("number_of_pages")){
+                        binding.tvPageCount.setText(Integer.toString(response.getInt("number_of_pages")) + " pages");
+                    }
+                }
+
+                catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+
     }
 
 
